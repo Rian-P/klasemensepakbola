929461fdc183c786de87e774789b07824680200d
Rian-P <57676700+Rian-P@users.noreply.github.com>
2023-12-26 10:50:14 +0700

    klasemen

--- resources/views/landing/layouts/app.blade.php
+++ resources/views/landing/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>aplikasi sederhana klasemen</title>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
